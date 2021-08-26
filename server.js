2d1d992f22c3efa65ee036a6d133f0780f6824c3
touchavin <touchavin@hotmail.com>
2021-08-26 20:43:42 +0700

    first commit

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 //Install express server
 const express = require('express');
-// Requirement of path Module in order to manage URLS
 const path = require('path');
+
 const app = express();
+
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/demo-deploy'));
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/demo-deploy/index.html'));
-});
+app.use(express.static('./dist/angular-app-heroku'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', { root: 'dist/angular-app-heroku/' }),
+);
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
