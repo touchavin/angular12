f5408d3e8e1d48a155804975563685ef9e69fe77
touchavin <touchavin@hotmail.com>
2021-08-26 20:54:26 +0700

    first commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/angular-app-heroku'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/https://angular12demo.herokuapp.com/' }),
+    res.sendFile('index.html', { root: 'dist/https://angular12demo/' }),
 );
 
 // Start the app by listening on the default Heroku port
