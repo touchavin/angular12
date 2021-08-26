c1582f7ca85bc092f7e6666aac2a3edd6e4ed48b
touchavin <touchavin@hotmail.com>
2021-08-26 20:59:30 +0700

    first commit

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/angular-app-heroku'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/https://angular12demo/' }),
-);
-
+app.get('*', (req, res) => {
+    res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+});
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
