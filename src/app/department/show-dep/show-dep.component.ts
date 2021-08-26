54784cef2aae42aa811aa2e5758aaf4d90928f47
touchavin <touchavin@hotmail.com>
2021-08-26 12:15:39 +0700

    first commit

---
+++ src/app/department/show-dep/show-dep.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
+
+@Component({
+  selector: 'app-show-dep',
+  templateUrl: './show-dep.component.html',
+  styleUrls: ['./show-dep.component.css']
+})
+export class ShowDepComponent implements OnInit {
+
+  constructor(private service:SharedService) { }
+
+  DepartmentList:any=[];
+
+
+  ModalTitle: string | undefined;
+
+  ActivateAddEditDepComp:boolean=false;
+  dep:any;
+
+  DepartmentIdFilter:string="";
+  DepartmentNameFilter:string="";
+  DepartmentListWithoutFilter:any=[];
+
+  ngOnInit(): void {
+    this.refreshDepList();
+  }
+
+
+  addClick(){
+    this.dep={
+      DepartmentId:0,
+      DepartmentName:""
+    }
+    this.ModalTitle="Add Department";
+    this.ActivateAddEditDepComp=true;
+
+  }
+  editClick(item:any){
+    this.dep=item;
+    this.ModalTitle="Edit Department";
+    this.ActivateAddEditDepComp=true;
+  }
+  deleteClick(item:any){
+    if(confirm("Are you sure")){
+      this.service.deleteDepartment(item.DepartmentId).subscribe(data =>{
+        alert(data.toString());
+        this.refreshDepList();
+      })
+    }
+  }
+
+  closeClick(){
+    this.ActivateAddEditDepComp=false;
+    this.refreshDepList();
+  }
+
+  refreshDepList(){
+    this.service.getDepList().subscribe(data=>{
+      this.DepartmentList=data;
+      this.DepartmentListWithoutFilter=data;
+    });
+  }
+
+}
+
